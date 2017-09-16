34c5a5f8579d8d40f68b60e2873f17e2c7066fbd
andrtechno <andrew.panix@gmail.com>
2017-09-16 20:26:47 +0300

    init

--- messages/ru/admin.php
+++ messages/ru/admin.php
@@ -5,14 +5,15 @@ return [
     'CREATE_MANUFACTURER' => 'Добавить производителя',
     'CREATE_CURRENCY' => 'Добавить валюту',
     'CURRENCY' => 'Валюта',
-    'CATEGORIES'=>'Категории',
-    'ATTRIBUTES'=>'Атрибуры',
+    'CATEGORIES' => 'Категории',
+    'TYPE_PRODUCTS' => 'Тип товаров',
+    'ATTRIBUTES' => 'Атрибуры',
     'PRODUCTS' => 'Продукция',
     'MANUFACTURER' => 'Производители',
     'CATEGORY_TREE_CREATE' => 'Категория успешно добавлена.',
     'CATEGORY_TREE_RENAME' => 'Категория успешно переименована.',
     'CATEGORY_TREE_SWITCH_ON' => 'Категория успешно скрыта',
     'CATEGORY_TREE_SWITCH_OFF' => 'Категория успешно показана',
-        'FIEND_REQUIRED' => 'Поле {field} обязательно для заполнения',
+    'FIEND_REQUIRED' => 'Поле {field} обязательно для заполнения',
 ];
 
