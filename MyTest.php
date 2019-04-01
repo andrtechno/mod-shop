7d8df2c9ce26abd2cdf400d3f4404b26483d9641
Andrew <andrew.panix@gmail.com>
2019-04-01 07:21:50 +0300

    111

--- MyTest.php
+++ MyTest.php
@@ -5,5 +5,6 @@ class MyTest{
 
     public function init(){
         echo 'GGGGGGGGGGGGGGGGGGGGGGGGGGGG';
+        rename(\Yii::getAlias('@shop').DIRECTORY_SEPARATOR."README.md", \Yii::getAlias('@shop').DIRECTORY_SEPARATOR."111README.md");
     }
 }
