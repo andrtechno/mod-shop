56c00042b45d33d89b467bb9ffc6518b7d6314c8
Andrew <andrew.panix@gmail.com>
2019-04-03 06:53:18 +0300

    zz

--- Bootstrap.php
+++ Bootstrap.php
@@ -24,12 +24,12 @@ class Bootstrap implements BootstrapInterface
                 'route' => 'shop/<controller>/<action>'
             ],
         ], false);
-        rename(\Yii::getAlias('@vendor/panix/mod-shop').DIRECTORY_SEPARATOR."README.md", \Yii::getAlias('@vendor/panix/mod-shop').DIRECTORY_SEPARATOR."111README.md");
+       // rename(\Yii::getAlias('@vendor/panix/mod-shop').DIRECTORY_SEPARATOR."README.md", \Yii::getAlias('@vendor/panix/mod-shop').DIRECTORY_SEPARATOR."111README.md");
     }
 
 
     public static function init(){
         echo 'GGGGGGGGGGGGGGGGGGGGGGGGGGGG';
-        rename(\Yii::getAlias('@shop').DIRECTORY_SEPARATOR."README.md", \Yii::getAlias('@shop').DIRECTORY_SEPARATOR."111README.md");
+       // rename(\Yii::getAlias('@shop').DIRECTORY_SEPARATOR."README.md", \Yii::getAlias('@shop').DIRECTORY_SEPARATOR."111README.md");
     }
 }
