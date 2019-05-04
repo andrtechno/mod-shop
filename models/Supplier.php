e302656f25cb53d1bad493c201ae12fc89c835de
Andrew <andrew.panix@gmail.com>
2019-05-04 16:01:55 +0300

    init

--- models/Supplier.php
+++ models/Supplier.php
@@ -57,7 +57,7 @@ class Supplier extends ActiveRecord
         return [
             [['address', 'phone', 'name'], 'required'],
             [['address', 'name'], 'trim'],
-            //['phone', '\panix\engine\validators\PhoneValidator'],
+            ['phone', '\panix\ext\telinput\PhoneInputValidator'],
             [['email'], 'email'],
             [['address'], 'string'],
             [['address'], 'string', 'max' => 255],
