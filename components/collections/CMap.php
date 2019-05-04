2ccedfbb4b3f52e315d95474876ac0359b74e081
Andrew <andrew.panix@gmail.com>
2019-05-04 17:55:20 +0300

    init

--- components/collections/CMap.php
+++ components/collections/CMap.php
@@ -5,6 +5,7 @@ namespace panix\mod\shop\components\collections;
 
 use Yii;
 use yii\base\Exception;
+use yii\helpers\ArrayHelper;
 
 class CMap extends \yii\base\Component implements \ArrayAccess, \Countable {
 
@@ -190,9 +191,9 @@ class CMap extends \yii\base\Component implements \ArrayAccess, \Countable {
                     $d = array();
                     foreach ($data as $key => $value)
                         $d[$key] = $value;
-                    $this->_d = self::mergeArray($this->_d, $d);
+                    $this->_d = ArrayHelper::merge($this->_d, $d);
                 } else
-                    $this->_d = self::mergeArray($this->_d, $data);
+                    $this->_d = ArrayHelper::merge($this->_d, $data);
             }
             else {
                 foreach ($data as $key => $value)
@@ -200,37 +201,6 @@ class CMap extends \yii\base\Component implements \ArrayAccess, \Countable {
             }
         } elseif ($data !== null)
             throw new Exception(Yii::t('yii', 'Map data must be an array or an object implementing Traversable.'));
-    }
-
-    /**
-     * Merges two or more arrays into one recursively.
-     * If each array has an element with the same string key value, the latter
-     * will overwrite the former (different from array_merge_recursive).
-     * Recursive merging will be conducted if both arrays have an element of array
-     * type and are having the same key.
-     * For integer-keyed elements, the elements from the latter array will
-     * be appended to the former array.
-     * @param array $a array to be merged to
-     * @param array $b array to be merged from. You can specify additional
-     * arrays via third argument, fourth argument etc.
-     * @return array the merged array (the original arrays are not changed.)
-     * @see mergeWith
-     */
-    public static function mergeArray($a, $b) {
-        $args = func_get_args();
-        $res = array_shift($args);
-        while (!empty($args)) {
-            $next = array_shift($args);
-            foreach ($next as $k => $v) {
-                if (is_integer($k))
-                    isset($res[$k]) ? $res[] = $v : $res[$k] = $v;
-                elseif (is_array($v) && isset($res[$k]) && is_array($res[$k]))
-                    $res[$k] = self::mergeArray($res[$k], $v);
-                else
-                    $res[$k] = $v;
-            }
-        }
-        return $res;
     }
 
     /**
