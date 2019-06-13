9ba24c3058911a6f616079ef75001d82fca4beab
Andrew <andrew.panix@gmail.com>
2019-06-13 12:03:11 +0300

    init

--- views/product/tabs/_video.php
+++ views/product/tabs/_video.php
@@ -1,1 +1,9 @@
-VIDEO;
+<?php
+use panix\engine\CMS;
+
+//API https://developers.google.com/youtube/player_parameters?hl=ru
+?>
+
+<div class="embed-responsive embed-responsive-16by9">
+    <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/<?= CMS::parse_yturl($model->video); ?>?autoplay=0&controls=1&rel=0" allowfullscreen></iframe>
+</div>
