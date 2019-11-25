4fa7d9daae0a8354d48cb9cd38bca043368169d4
Andrew <andrew.panix@gmail.com>
2019-11-25 21:43:32 +0200

    init

--- views/admin/product/index.php
+++ views/admin/product/index.php
@@ -6,10 +6,10 @@ use panix\mod\shop\bundles\admin\ProductIndex;
 
 echo Fancybox::widget(['target' => '.image a']);
 
-Pjax::begin([
-    'id' => 'pjax-grid-product',
-]);
-ProductIndex::register($this);
+//Pjax::begin([
+//    'id' => 'pjax-grid-product',
+//]);
+//ProductIndex::register($this);
 echo GridView::widget([
     'id' => 'grid-product',
     'tableOptions' => ['class' => 'table table-striped'],
@@ -29,24 +29,5 @@ echo GridView::widget([
     //   'footerRowOptions' => ['class' => 'text-center'],
     'rowOptions' => ['class' => 'sortable-column']
 ]);
-Pjax::end();
-?>
+//Pjax::end();
 
-<!--<button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal" data-whatever="@getbootstrap">get</button>
-
-<div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-    <div class="modal-dialog" role="document">
-        <div class="modal-content">
-            <div class="modal-header">
-                <h5 class="modal-title" id="exampleModalLabel">New message</h5>
-                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                    <span aria-hidden="true">&times;</span>
-                </button>
-            </div>
-            <div class="modal-body">
-                test
-            </div>
-
-        </div>
-    </div>
-</div>-->
