ec82fd15f2b6efc83e302b2b5f76f15c137c2cfa
Andrew <andrew.panix@gmail.com>
2019-11-27 01:31:48 +0200

    init

--- migrations/m180917_193650_shop_product_attribute_eav.php
+++ migrations/m180917_193650_shop_product_attribute_eav.php
@@ -20,7 +20,7 @@ class m180917_193650_shop_product_attribute_eav extends Migration
         $this->createTable('{{%shop__product_attribute_eav}}', [
             'entity' => $this->integer()->unsigned(),
             'attribute' => $this->string(255)->null(),
-            'value' => $this->integer()->unsigned(),
+            'value' => $this->text(),
         ], $this->tableOptions);
 
         $this->createIndex('entity', '{{%shop__product_attribute_eav}}', 'entity');
