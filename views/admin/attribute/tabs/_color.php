e5836b28874801f169b23633e0bbd4d298765d10
Andrew <andrew.panix@gmail.com>
2019-12-22 13:13:49 +0200

    init

--- views/admin/attribute/tabs/_color.php
+++ views/admin/attribute/tabs/_color.php
@@ -25,25 +25,6 @@ $this->registerCss('
 
 ?>
 
-<div class="jumbotron">
-    <h1>Bootstrap Colorpicker Demo</h1>
-    <input id="demo" type="text" class="form-control" value="rgb(255, 128, 0)" />
-</div>
-<?php $this->registerJsFile(); ?>
-<script src="//code.jquery.com/jquery-3.3.1.js"></script>
-<script src="//cdn.rawgit.com/twbs/bootstrap/v4.1.3/dist/js/bootstrap.bundle.min.js"></script>
-<script src="dist/js/bootstrap-colorpicker.js"></script>
-<script>
-    $(function () {
-        // Basic instantiation:
-        $('#demo').colorpicker();
-
-        // Example using an event, to change the color of the .jumbotron background:
-        $('#demo').on('colorpickerChange', function(event) {
-            $('.jumbotron').css('background-color', event.color.toString());
-        });
-    });
-</script>
 <table>
     <tr class="copyMe">
         <td class="text-center">&mdash;</td>
