29b8ed814b150077ec784eb7def16f41564ed393
Andrew <andrew.panix@gmail.com>
2020-01-25 00:31:22 +0200

    init

--- models/Product.php
+++ models/Product.php
@@ -323,7 +323,7 @@ class Product extends ActiveRecord
         $rules[] = [['full_description', 'discount'], 'string'];
         $rules[] = ['use_configurations', 'boolean', 'on' => self::SCENARIO_INSERT];
         $rules[] = ['enable_comments', 'boolean'];
-        $rules[] = [['sku', 'full_description', 'unit', 'video', 'price_purchase', 'label'], 'default']; // установим ... как NULL, если они пустые
+        $rules[] = [['sku', 'full_description', 'video', 'price_purchase', 'label'], 'default']; // установим ... как NULL, если они пустые
         $rules[] = [['price', 'price_purchase'], 'double'];
         $rules[] = [['manufacturer_id', 'type_id', 'quantity', 'views', 'availability', 'added_to_cart_count', 'ordern', 'category_id', 'currency_id', 'supplier_id', 'label'], 'integer'];
         $rules[] = [['name', 'slug', 'full_description', 'use_configurations'], 'safe'];
