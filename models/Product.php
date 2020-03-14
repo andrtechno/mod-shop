ff84ba5192da99a1f7980797025cfe1b7e8aef66
Andrew <andrew.panix@gmail.com>
2020-03-14 09:07:00 +0200

    init

--- models/Product.php
+++ models/Product.php
@@ -790,13 +790,8 @@ class Product extends ActiveRecord
         ]);
 
 
-        //  $this->removeImages();
-        $image = $this->getImages();
+		$this->removeImages();
 
-        if ($image) {
-            //get path to resized image
-            $this->removeImages();
-        }
         // Clear configurable attributes
         Yii::$app->db->createCommand()->delete('{{%shop__product_configurable_attributes}}', ['product_id' => $this->id])->execute();
         // Delete configurations
