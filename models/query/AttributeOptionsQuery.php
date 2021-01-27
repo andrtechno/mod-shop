aa19a84567afb493731a639a4d6aa121bb19e513
Andrew <andrew.panix@gmail.com>
2021-01-27 19:09:58 +0200

    init

--- models/query/AttributeOptionsQuery.php
+++ models/query/AttributeOptionsQuery.php
@@ -16,14 +16,6 @@ class AttributeOptionsQuery extends ActiveQuery
 
     use DefaultQueryTrait;
 
-    public function init()
-    {
-        /** @var \yii\db\ActiveRecord $modelClass */
-        $modelClass = $this->modelClass;
-        $tableName = $modelClass::tableName();
-        if (isset($modelClass::getTableSchema()->columns['ordern'])) {
-            $this->addOrderBy(["{$tableName}.ordern" => SORT_DESC]);
-        }
-        parent::init();
-    }
+    public $defaultSort=true;
+
 }
