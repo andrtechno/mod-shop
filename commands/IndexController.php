31fc27b336dffffb939eb03706d6d66c119cf556
Andrew <andrew.panix@gmail.com>
2021-02-11 09:50:19 +0200

    load

--- commands/IndexController.php
+++ commands/IndexController.php
@@ -2,9 +2,12 @@
 
 namespace panix\mod\shop\commands;
 
+use panix\engine\CMS;
 use panix\mod\images\models\Image;
 use Yii;
 use yii\helpers\Console;
+use yii\helpers\Html;
+use yii\helpers\StringHelper;
 use yii\helpers\Url;
 use panix\mod\shop\components\SimpleXMLExtended;
 use panix\mod\shop\models\Product;
@@ -19,37 +22,45 @@ class IndexController extends ConsoleController
 {
 
     /**
-     * generate google feed XML
+     * Generate google feed XML.
+     * @param string $host Host for url
+     * @param integer $minify Minimization file content: use 1 or 0
+     * @return mixed
      */
-    public function actionGoogleFeed()
+    public function actionGoogleFeed($host = 'no-host.com', $minify = 1)
     {
-        //https://support.google.com/merchants/answer/6324484?hl=ru&ref_topic=6324338
+        //https://support.google.com/merchants/answer/6324484?hl=ru
         $ns = 'http://base.google.com/ns/1.0';
-        $xml = new SimpleXMLExtended('<rss xmlns:g="' . $ns . '" version="2.0" />', 0, false, "g", 'http://base.google.com/ns/1.0');
+        $fullURL = 'https://' . $host;
+        $xml = new SimpleXMLExtended('<rss xmlns:g="' . $ns . '" version="2.0" />');
+
+        $main_iso = Yii::$app->currency->main['iso'];
+        $currencies = Yii::$app->currency->getCurrencies();
+
         $channel = $xml->addChild('channel');
 
-
-        $channel->addChildWithCDATA('link', 'https://chika.in.ua');
+        $channel->addChildWithCDATA('link', $fullURL);
         $channel->addChildWithCDATA('title', Yii::$app->settings->get('app', 'sitename'));
         $channel->addChildWithCDATA('description', Yii::$app->settings->get('app', 'sitename'));
         $products = Product::find()
-           // ->limit(100)
-           // ->where(['switch' => 1])
+            //->limit(10)
+            // ->where(['switch' => 1])
             //->andWhere(['use_configurations' => 1])
             //->andWhere(['availability'=>1])
+            //->andWhere(['id' => 8812])
             ->all();
-        $count=count($products);
-        $i=0;
+        $count = count($products);
+        $i = 0;
         Console::startProgress($i, $count);
 
         foreach ($products as $i => $product) {
             $item = $channel->addChild('item');
             $item->addChild('id', $product->id, $ns);
-            $item->addChildWithCDATA('title', $product->name, $ns);
-            $item->addChildWithCDATA('description', $product->full_description, $ns);
+            $item->addChildWithCDATA('title', StringHelper::truncate($product->name, 150 - 3), $ns);
+            $item->addChildWithCDATA('description', StringHelper::truncate($product->full_description, 5000 - 3), $ns);
 
 
-            $item->addChild('link', Url::to($product->getUrl(), true), $ns);
+            $item->addChild('link', $fullURL . Url::to($product->getUrl()), $ns);
 
 
             $images = Image::find()
@@ -60,16 +71,41 @@ class IndexController extends ConsoleController
 
                 if (file_exists(Yii::getAlias($image->path) . DIRECTORY_SEPARATOR . $image->filePath)) {
                     if ($image->is_main) {
-                        $item->addChild('image_link', Url::to('/uploads/store/product/' . $image->filePath, true), $ns);
+                        $item->addChild('image_link', $fullURL . '/uploads/store/product/' . $image->filePath, $ns);
                     } else {
-                        $item->addChild('additional_image_link', '/uploads/store/product/' . $image->filePath, $ns);
+                        $item->addChild('additional_image_link', $fullURL . '/uploads/store/product/' . $image->filePath, $ns);
                     }
                 }
 
             }
 
 
-            $item->addChild('price', $product->price . " UAH", $ns);
+            $custom_label=0;
+            foreach ($product->labels() as $key => $label) {
+                $item->addChild('custom_label_'.$custom_label, $label['label'], $ns);
+                $custom_label++;
+            }
+
+            //$priceValue = '';
+            //$priceSymbol = '';
+            if ($product->currency_id) {
+                $priceValue = $product->price * $currencies[$product->currency_id]['rate'];
+            } else {
+                $priceValue = $product->price;
+            }
+
+
+
+            if ($product->discount) {
+                $sum = $product->discount;
+                if ('%' === substr($sum, -1, 1)) {
+                    $sum = $priceValue * ((double)$sum) / 100;
+                }
+                $discountPrice = $priceValue - $sum;
+                $item->addChild('price_sale', (($product->currency_id) ? $discountPrice * $currencies[$product->currency_id]['rate'] : $discountPrice) . " " . $main_iso, $ns);
+            }
+
+            $item->addChild('price', $priceValue . " " . $main_iso, $ns);
             $item->addChild('condition', "new", $ns);
 
 
@@ -89,19 +125,19 @@ class IndexController extends ConsoleController
             if ($product->manufacturer_id) {
                 $brand = $product->manufacturer;
                 if ($brand) {
-                    $item->addChild('brand', $brand->name, $ns);
+                    $item->addChild('brand', StringHelper::truncate($brand->name, 70 - 3), $ns);
                 }
             }
 
             //Bonus program
             $loyalty_points = $item->addChild('loyalty_points', null, $ns);
-            $loyalty_points->addChild('name', "Бонусная программа", $ns);
-            $loyalty_points->addChild('points_value', $product->price * Yii::$app->settings->get('user', 'bonus_ratio'), $ns);
+            $loyalty_points->addChild('name', StringHelper::truncate("Бонусная программа", 15 - 3), $ns);
+            $loyalty_points->addChild('points_value', floor($priceValue * Yii::$app->settings->get('user', 'bonus_ratio')), $ns);
             $loyalty_points->addChild('ratio', Yii::$app->settings->get('user', 'bonus_ratio'), $ns);
 
 
             //Доставка
-            $shipping = $item->addChild('shipping', null, $ns);
+            /*$shipping = $item->addChild('shipping', null, $ns);
             $shipping->addChild('country', "US", $ns);
             $shipping->addChild('region', "MA", $ns);
             $shipping->addChild('service', "Наземная доставка", $ns);
@@ -111,24 +147,25 @@ class IndexController extends ConsoleController
             $shipping->addChild('region', "UA", $ns);
             $shipping->addChild('postal_code', 65000, $ns);
             $shipping->addChild('service', "Новая почта", $ns);
-            $shipping->addChild('price', "65.00 UAH", $ns);
+            $shipping->addChild('price', "65.00 UAH", $ns);*/
 
 
             if ($product->use_configurations) {
                 $configuration = $product->getConfigurations(true);
                 if ($configuration) {
                     sort($configuration); //generate unique hash configuration
-                    $item->addChild('item_group_id', implode('-', $configuration), $ns);
+                    $item->addChild('item_group_id', CMS::hash(implode('-', $configuration)), $ns);
                 }
             }
 
 
             $item->addChild('ships_from_country', 'UA', $ns);
-
-
             foreach ($product->getDataAttributes() as $data) {
                 foreach ($data as $key => $attribute) {
-                    $item->addChild($key, $attribute['value'], $ns);
+                    $product_detail = $item->addChild('product_detail', null, $ns);
+                    //$product_detail->addChild('section_name', 'Общие сведения', $ns);
+                    $product_detail->addChild('attribute_name', $attribute['name'], $ns);
+                    $product_detail->addChild('attribute_value', $attribute['value'], $ns);
                 }
 
             }
@@ -137,7 +174,19 @@ class IndexController extends ConsoleController
 
         }
         Console::endProgress(false);
-        return $xml->saveXML(Yii::getAlias('@runtime').DIRECTORY_SEPARATOR.'google-feed.xml');
+        // $xml = html_entity_decode($xml, ENT_NOQUOTES, 'UTF-8');
+
+        $dom = new \DOMDocument();
+        $dom->loadXML($xml->asXML());
+        $dom->encoding = 'UTF-8';
+        if(!$minify){
+            $dom->preserveWhiteSpace = false;
+            $dom->formatOutput = true;
+        }
+        $xml = $dom->saveXML();
+        $xml = new SimpleXMLExtended($xml, 0, false, "g", 'http://base.google.com/ns/1.0');
+
+        return $xml->saveXML(Yii::getAlias('@runtime') . DIRECTORY_SEPARATOR . 'google-feed.xml');
     }
 
 
