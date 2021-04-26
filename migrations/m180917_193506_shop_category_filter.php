00c2c200c10f9f29a282f9760de24af5c15610de
Andrew <andrew.panix@gmail.com>
2021-04-26 10:51:22 +0300

    fix m180917_193506_shop_category_filter

--- migrations/m180917_193506_shop_category_filter.php
+++ migrations/m180917_193506_shop_category_filter.php
@@ -3,7 +3,7 @@
 /**
  * Generation migrate by PIXELION CMS
  * @author PIXELION CMS development team <dev@pixelion.com.ua>
- * 
+ *
  * Class m180917_193506_shop_category_filter
  */
 
@@ -11,20 +11,17 @@ use panix\engine\db\Migration;
 use panix\mod\shop\models\CategoryFilter;
 
 
-class m180917_193506_shop_category_filter extends Migration {
+class m180917_193506_shop_category_filter extends Migration
+{
 
     public function up()
     {
         $this->createTable(CategoryFilter::tableName(), [
-            'id' => $this->primaryKey()->unsigned(),
             'category_id' => $this->integer()->unsigned()->null(),
             'option_id' => $this->integer()->unsigned()->null(),
         ]);
 
-
-        $this->createIndex('option_id', CategoryFilter::tableName(), 'option_id');
-        $this->createIndex('category_id', CategoryFilter::tableName(), 'category_id');
-
+        $this->addPrimaryKey('{{%shop__category_filter_pk}}', CategoryFilter::tableName(), ['category_id', 'option_id']);
     }
 
     public function down()
