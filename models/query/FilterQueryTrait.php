4d147e7258dc6f71da0ffe65145812274f6eaab0
Andrew <andrew.panix@gmail.com>
2023-06-27 19:58:35 +0300

    fix query price

--- models/query/FilterQueryTrait.php
+++ models/query/FilterQueryTrait.php
@@ -20,23 +20,37 @@ trait FilterQueryTrait
                         {$tableName}.`price`
                 END) AS aggregation_price"]);*/
 
-
-        $this->select(["{$function}(CASE WHEN ({$tableName}.`currency_id`)
+        if (Yii::$app->db->driverName === 'pgsql') {
+            $this->select(["{$function}(CASE WHEN ({$tableName}.currency_id IS NOT NULL)
                     THEN
-                        (CASE WHEN ({$tableName}.`discount`) THEN
-                         ({$tableName}.`price` * ((SELECT rate FROM {$tableNameCur} WHERE {$tableNameCur}.`id`={$tableName}.`currency_id`) - {$tableName}.`discount`))
+                        (CASE WHEN ({$tableName}.discount::boolean) THEN
+                         (({$tableName}.price - discount::numeric) * (SELECT rate::numeric FROM {$tableNameCur} WHERE id=currency_id))
                          ELSE
-                          ({$tableName}.`price` * (SELECT rate FROM {$tableNameCur} WHERE {$tableNameCur}.`id`={$tableName}.`currency_id`))
+                          ({$tableName}.price * (SELECT rate::numeric FROM {$tableNameCur} WHERE id=currency_id))
                          END)
                         
                     ELSE
-                        (CASE WHEN ({$tableName}.`discount`) THEN ({$tableName}.`price` - {$tableName}.`discount`) ELSE {$tableName}.`price` END)
+                        (CASE WHEN ({$tableName}.discount::boolean) THEN (({$tableName}.price::numeric - {$tableName}.discount::numeric)) ELSE {$tableName}.price::numeric END)
                 END) AS aggregation_price"]);
+        }else{
+            $this->select(["{$function}(CASE WHEN ({$tableName}.currency_id IS NOT NULL)
+                    THEN
+                        (CASE WHEN ({$tableName}.discount) THEN
+                         (({$tableName}.price - {$tableName}.discount) * (SELECT rate FROM {$tableNameCur} WHERE {$tableNameCur}.id={$tableName}.currency_id))
+                         ELSE
+                          ({$tableName}.price * (SELECT rate FROM {$tableNameCur} WHERE {$tableNameCur}.id={$tableName}.currency_id))
+                         END)
+                        
+                    ELSE
+                        (CASE WHEN ({$tableName}.discount) THEN ({$tableName}.price - {$tableName}.discount) ELSE {$tableName}.price END)
+                END) AS aggregation_price"]);
+        }
+
 
         //$this->orderBy(["aggregation_price" => ($function === 'MIN') ? SORT_ASC : SORT_DESC]);
         $this->distinct(false);
         $this->limit(1);
-       //echo $this->createCommand()->rawSql;die;
+        //echo $this->createCommand()->rawSql;die;
         return $this;
     }
 
